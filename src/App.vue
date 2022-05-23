20fe476b98ace9ee129dc61c7163f7fab7a4c2cf
Mukhtarov-Ramazan <mukhtarovramazan239@gmail.com>
2022-05-23 20:12:01 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <component :is="layout">
+      <router-view/>
+    </component>
+  </div>
+</template>
+
+<script>
+import EmptyLayout from '@/layouts/EmptyLayout.vue'
+import MainLayout from '@/layouts/MainLayout.vue'
+
+export default {
+  components: {
+    EmptyLayout,
+    MainLayout
+  },
+
+  computed: {
+    layout() {
+      return (this.$route.meta.layout || 'empty') + '-layout'
+    }
+  }
+}
+</script>
+
+
+<style>
+@import '~materialize-css/dist/css/materialize.min.css';
+@import './assets/css/index.css';
+</style>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="app-main-layout">
+
+    <navbar @clickBurger="isOpen = !isOpen"/>
+
+    <sidebar v-model="isOpen"/>
+    
+    <main class="app-content" :class="{full: !isOpen}">
+      <div class="app-page">
+        <router-view/>
+      </div>
+    </main>
+
+    <div class="fixed-action-btn">
+      <router-link class="btn-floating btn-large blue" to="/record">
+        <i class="large material-icons">add</i>
+      </router-link>
+    </div>
+  </div>
+
+</template>
+
+<script>
+import Navbar from '../components/app/Navbar.vue';
+import Sidebar from '../components/app/Sidebar.vue';
+export default {
+  components: { Navbar, Sidebar },
+
+  data() {
+    return{
+      isOpen: true
+    }
+  }
+}
+</script>
+
+<style></style>
